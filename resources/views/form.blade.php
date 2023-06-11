0f3a579e972769f90d4c758537f8606ad05f12aa
Hassan Gomaa <hassangomaa974@gmail.com>
2023-06-11 16:32:32 +0300

    design v3 : using app.blade

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,33 +1,6 @@
 @extends("layouts.app")
 
 @section("content")
-    <!-- ================================ top header ============================== -->
-    <div class="top-header">
-        <p>
-            THE KPI INSTITUTE
-        </p>
-    </div>
-
-    <div class="main-content-Navigation-container">
-        <div class="main-content-Navigation">
-            <div class="main-content-Navigation-img">
-                <img src="./downloads/Icon1_19.png" alt="">
-                <p>
-                    TKI RESEARCH LIBRARY
-                </p>
-            </div>
-
-            <div class="main-content-Navigation-auth">
-                <p>
-                    <a href="">Login</a>
-                </p>
-                <p>
-                    <a href="">Register</a>
-                </p>
-            </div>
-
-        </div>
-    </div>
 
 
     <!-- ================================ main content ============================== -->
