0823d2ba35ac9ce179b22a151021752d342b3a3c
Hassan Gomaa <hassangomaa974@gmail.com>
2023-06-11 15:38:58 +0300

    design v2 & responsive

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,32 +1,256 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>TKI Library Membership Form</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-<body>
-<div class="container">
-    <h1>TKI Library Membership Form</h1>
-    <form method="POST" action="{{ route('submit-form') }}">
-        @csrf
-        <div class="form-group">
-            <label for="name">Name:</label>
-            <input type="text" name="name" id="name" required>
+@extends("layouts.app")
+
+@section("content")
+    <!-- ================================ top header ============================== -->
+    <div class="top-header">
+        <p>
+            THE KPI INSTITUTE
+        </p>
+    </div>
+
+    <div class="main-content-Navigation-container">
+        <div class="main-content-Navigation">
+            <div class="main-content-Navigation-img">
+                <img src="./downloads/Icon1_19.png" alt="">
+                <p>
+                    TKI RESEARCH LIBRARY
+                </p>
+            </div>
+
+            <div class="main-content-Navigation-auth">
+                <p>
+                    <a href="">Login</a>
+                </p>
+                <p>
+                    <a href="">Register</a>
+                </p>
+            </div>
+
         </div>
-        <div class="form-group">
-            <label for="email">Email Address:</label>
-            <input type="email" name="email" id="email" required>
+    </div>
+
+
+    <!-- ================================ main content ============================== -->
+    <div class="container">
+        <div class="main-content">
+            <div class="main-content-header">
+                <h1>
+                    The KPI Dictionary Volume I: Functional Areas
+                </h1>
+
+            </div>
+
+
+            <div class="main-comtent-second-header">
+                3,200+ Key Performance Indicator Definitions
+                <br>
+                for an in depth view on performance measurement
+            </div>
+
+            <div class="main-content-paragraph">
+                <span
+                    style="font-family: 'Finlandica', sans-serif; font-size: 20px; font-weight: 700; color: #000000;">The
+                    KPI Dictionary - Volume 1</span>
+                is a reference collection of Key Performance Indicators, used in practice
+                today, which
+                covers the major functional areas
+                of an organization, regardless of its industry specifics. The collection contains the most relevant
+                means for measuring
+                performance, throughout the most
+                common departments in either public, private or non-profit organizations, such as: Finance, Sales, Human
+                Resources,
+                Marketing and IT.
+            </div>
+
+            <div class="main-content-download-containrt">
+                <div class="main-content-download-containrt-left">
+                    <div class="main-content-download-containrt-left-header">
+                        <img src="./downloads/Icon1_03.png" alt="">
+                        <p>
+                            KEY BENEFITS OF THE FUNCTIONAL AREAS KPI DICTIONARY
+                        </p>
+
+                    </div>
+                    <div class="main-content-download-containrt-left-description">
+                        <div>
+                            <div class="main-content-download-containrt-left-description-item">
+                                <p>
+                                    <span> SELECT </span>
+                                    -the critical KPIs to drive operational
+                                    success
+                                </p>
+
+
+                            </div>
+                            <div class="main-content-download-containrt-left-description-item">
+                                <p>
+                                    <span> SELECT </span>
+                                    -the critical KPIs to drive operational
+                                    success
+                                </p>
+
+
+                            </div>
+                            <div class="main-content-download-containrt-left-description-item">
+                                <p>
+                                    <span> SELECT </span>
+                                    -the critical KPIs to drive operational
+                                    success
+                                </p>
+
+
+                            </div>
+                            <div class="main-content-download-containrt-left-description-item">
+                                <p>
+                                    <span> SELECT </span>
+                                    -the critical KPIs to drive operational
+                                    success
+                                </p>
+
+
+                            </div>
+                        </div>
+                        <div>
+                            <img src="./downloads/Icon1_07.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="main-content-download-containrt-right">
+
+                    <div class="main-content-download-containrt-right-form">
+                        <h2>
+                            Download the free sample
+                        </h2>
+                        <p>
+                            To proceed with your order, please take
+                            a moment to fill in the fields below: </p>
+                        <form action="">
+                            <input type="text" placeholder="First Name">
+                            <input type="text" placeholder="Last Name">
+                            <input type="text" placeholder="Company">
+                            <input type="text" placeholder="Cantry">
+                            <div>
+                                <input type="text" placeholder="Prefix">
+                                <input type="text" placeholder="Phone Number">
+                            </div>
+                            <input type="text" placeholder="Email Address">
+                        </form>
+                        <div class="main-content-download-containrt-right-form-footer">
+                            <p>
+                                We value your privacy and will never disclose your
+                                data to third parties without your consent.
+                            </p>
+                            <button>
+                                Download
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="phone">Phone Number:</label>
-            <input type="text" name="phone" id="phone" required>
+    </div>
+
+    <!-- =============================== TKI FACTS AND FIGURES ===================== -->
+    <div class="main-figure-container">
+        <h2>
+            TKI FACTS AND FIGURES
+        </h2>
+        <div class="main-figure-container-content">
+
+            <div>
+                <img src="./downloads/Icon1_11.png" alt="">
+                <p>
+
+                    <span>75,000+</span>
+                    <span> COMMUNITY <br> MEMBERS</span>
+                </p>
+            </div>
+            <div>
+                <img src="./downloads/Icon1_11.png" alt="">
+                <p>
+
+                    <span>75,000+</span>
+                    <span> COMMUNITY <br> MEMBERS</span>
+                </p>
+            </div>
+            <div>
+                <img src="./downloads/Icon1_11.png" alt="">
+                <p>
+
+                    <span>75,000+</span>
+                    <span> COMMUNITY <br> MEMBERS</span>
+                </p>
+            </div>
+            <div>
+                <img src="./downloads/Icon1_11.png" alt="">
+                <p>
+
+                    <span>75,000+</span>
+                    <span> COMMUNITY <br> MEMBERS</span>
+                </p>
+            </div>
+            <div>
+                <img src="./downloads/Icon1_11.png" alt="">
+                <p>
+
+                    <span>75,000+</span>
+                    <span> COMMUNITY <br> MEMBERS</span>
+                </p>
+            </div>
+
+
         </div>
-        <button type="submit">Submit</button>
-    </form>
-</div>
-<script src="{{ asset('js/app.js') }}"></script>
-<script>
-    // Your JavaScript code for form validation and country autocomplete
-</script>
-</body>
-</html>
+    </div>
+
+    <!-- =============================== contact us ===================== -->
+    <div class="contactus-container">
+        <div class="contactus">
+            <div>
+                <div>
+                    <img src="./downloads/Icon1_15.png" alt="">
+
+                </div>
+                <div>
+                    <h2>
+                        CHAT WITH US </h2>
+                    <p>
+                        Have questions? A customer service
+                        representative is online to help you via
+                        Live Chat. Join in the conversation.
+                    </p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="./downloads/Icon1_15.png" alt="">
+
+                </div>
+                <div>
+                    <h2>
+                        CHAT WITH US </h2>
+                    <p>
+                        Have questions? A customer service
+                        representative is online to help you via
+                        Live Chat. Join in the conversation.
+                    </p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="./downloads/Icon1_15.png" alt="">
+
+                </div>
+                <div>
+                    <h2>
+                        CHAT WITH US </h2>
+                    <p>
+                        Have questions? A customer service
+                        representative is online to help you via
+                        Live Chat. Join in the conversation.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+@extends("layouts.app")
+
+@section("footer")
+<!-- =============================== footer ===================== -->
+
+
+<div class="footer">
+    Copyright © 2009-2015 The KPI Institute Ltd. All rights Reserved
+
+</div>
+@endsection
